7294f5bd47dcdb0736626eca10ff4a37867b0e87
infopek <10saero01@gmail.com>
2023-10-02 20:50:54 +0200

    Refactor main application

--- ImageBlur/src/application.cpp
+++ ImageBlur/src/application.cpp
@@ -1,10 +1,14 @@
-#include <iostream>
-
-#include "opencv2/opencv.hpp"
+#include <opencv2/opencv.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 
-#include "../cuda/blur.h"
+#include "../cuda/box_blur.h"
+#include "../cuda/median_filter.h"
+
+#include <iostream>
+#include <filesystem>
+
+namespace fs = std::filesystem;
 
 void show(const cv::Mat& image)
 {
@@ -12,56 +16,50 @@ void show(const cv::Mat& image)
 	cv::waitKey(0);
 }
 
-void blurCPU(unsigned char* src, unsigned char* dst, size_t width, size_t height, int kernelSize)
+void boxBlur(cv::Mat& image)
 {
-	for (int y = 0; y < height; y++)
-	{
-		for (int x = 0; x < width; x++)
-		{
-			int sum{};
-			int numPixels{};
-
-			for (int r = -kernelSize / 2; r <= kernelSize / 2; ++r)
-			{
-				for (int c = -kernelSize / 2; c <= kernelSize / 2; ++c)
-				{
-					int row = y + r;
-					int col = x + c;
-
-					if (row >= 0 && row < height && col >= 0 && col < width)
-					{
-						sum += src[row * width + col];
-						++numPixels;
-					}
-				}
-			}
-
-			dst[y * width + x] = (unsigned char)(sum / (kernelSize * kernelSize));
-		}
-	}
-}
-
-int main()
-{
-	cv::Mat image = cv::imread("./res/nature.jpg");
-	const int width = image.cols;
-	const int height = image.rows;
+	BoxBlur blurrer(image.cols, image.rows);
+	int kernelSize = 6;
 
 	std::vector<cv::Mat> channels;
 	cv::split(image, channels);
-
-	Blur blurrer(width, height);
-	int kernelSize = 9;
 
 	blurrer.blur(channels[2].data, channels[2].data, kernelSize);
 	blurrer.blur(channels[1].data, channels[1].data, kernelSize);
 	blurrer.blur(channels[0].data, channels[0].data, kernelSize);
 
-	/*blurCPU(channels[2].data, channels[2].data, width, height, kernelSize);
-	blurCPU(channels[1].data, channels[1].data, width, height, kernelSize);
-	blurCPU(channels[0].data, channels[0].data, width, height, kernelSize);*/
+	cv::merge(channels, image);
+}
+
+void medianFilter(cv::Mat& image)
+{
+	MedianFilter filterer(image.cols, image.rows);
+
+	std::vector<cv::Mat> channels;
+	cv::split(image, channels);
+
+	filterer.filter(channels[2].data, channels[2].data);
+	filterer.filter(channels[1].data, channels[1].data);
+	filterer.filter(channels[0].data, channels[0].data);
 
 	cv::merge(channels, image);
+}
+
+int main(int argc, char* argv[])
+{
+	fs::path dir("res");
+	fs::path file("nature.jpg");
+	fs::path fullPath = dir / file;
+
+	cv::Mat image = cv::imread(fullPath.string());
+
+	//show(image);
+	
+	//medianFilter(image);
+	boxBlur(image);
+
+	//cv::medianBlur(image, image, 3);
+	//cv::blur(image, image, cv::Size(5, 5));
 
 	show(image);
 }
